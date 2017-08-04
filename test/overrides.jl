9b9e99643a53f026b49f33c48ddca9d31efbd3ae
Seth Bromberger <github@bromberger.com>
2017-08-04 16:25:05 -0700

    changes signature of adjacency_matrix in concert with LightGraphs #713

--- test/overrides.jl
+++ test/overrides.jl
@@ -1,17 +1,18 @@
 @testset "Overrides" begin
     g3 = SimpleWeightedGraph(PathGraph(5))
     g5 = SimpleWeightedDiGraph(4)
+
     for g in testgraphs(g3)
-        @test @inferred(adjacency_matrix(g, Bool)) == adjacency_matrix(g, :out, Bool)
-        @test @inferred(adjacency_matrix(g))[3,2] == 1
-        @test adjacency_matrix(g)[2,4] == 0
-        @test adjacency_matrix(g, :out) == adjacency_matrix(g,:in)'
+        @test @inferred(adjacency_matrix(g, Bool)) == adjacency_matrix(g, Bool; dir=:out)
+        @test @inferred(adjacency_matrix(g))[3, 2] == 1
+        @test adjacency_matrix(g)[2, 4] == 0
+        @test adjacency_matrix(g; dir=:out) == adjacency_matrix(g; dir=:in)'
     end
 
-    add_edge!(g5,1,2, 2); add_edge!(g5,2, 3, 2); add_edge!(g5,1,3, 1); add_edge!(g5,3,4, 5)
+    add_edge!(g5, 1, 2, 2); add_edge!(g5, 2, 3, 2); add_edge!(g5, 1, 3, 1); add_edge!(g5, 3, 4, 5)
     for g in testdigraphs(g5)
-        @test @inferred(adjacency_matrix(g, Bool)) == adjacency_matrix(g, :out, Bool)
-        @test adjacency_matrix(g, :out) == adjacency_matrix(g,:in)'
+        @test @inferred(adjacency_matrix(g, Bool)) == adjacency_matrix(g, Bool; dir=:out)
+        @test adjacency_matrix(g; dir=:out) == adjacency_matrix(g; dir=:in)'
     end
 end
 
