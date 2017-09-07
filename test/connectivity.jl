aa9beaebe04d744fca733aee560db662d77f37ad
ranjanan <benditlikeranjan@gmail.com>
2017-09-08 02:56:10 +0530

    Fix tests

--- test/connectivity.jl
+++ test/connectivity.jl
@@ -20,24 +20,16 @@ const graphs = [
 :tetrahedral,
 :truncatedcube,
 :truncatedtetrahedron,
-:truncatedtetrahedron_dir,
 :tutte]                    
 @testset "Connectivity" begin
-    for s in graphs
-        g6 = smallgraph(s)
-        gx = PathGraph(4)
-        add_vertices!(gx, 10)
-        add_edge!(gx, 5, 6)
-        add_edge!(gx, 6, 7)
-        add_edge!(gx, 8, 9)
-        add_edge!(gx, 10, 9)
-
-
-        for g in testgraphs(gx)
-            a = adjacency_matrix(g)
-            x = connected_components(g)
-            y = connected_components(SimpleWeightedGraph(a))
-            @test x == y
-        end
-    end
-end
+     for s in graphs
+         gx = smallgraph(s)
+ 
+         for g in testgraphs(gx)
+             a = adjacency_matrix(g)
+             x = connected_components(g)
+             y = connected_components(SimpleWeightedGraph(a))
+             @test x == y
+         end
+     end
+ end
